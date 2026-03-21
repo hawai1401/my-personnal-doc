0f429ca950fa627333168c37b02cc000d872acd8
hawai1401 <hawai1401@ik.me>
2026-03-21 12:02:14 +0100

    feat: Added prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@/prisma/generated/prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
+
+const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL!,
+}).$extends(withAccelerate());
+
+export default prisma;
